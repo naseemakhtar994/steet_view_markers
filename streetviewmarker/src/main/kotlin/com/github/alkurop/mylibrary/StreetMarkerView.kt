ad2bf14e8c9f1f010e2f7f0b170b59e09142cb52
Alex Kurop <alkurop@gmail.com>
2017-04-07 14:39:47 +0300

    Add ui street marker view

--- streetviewmarker/src/main/kotlin/com/github/alkurop/mylibrary/StreetMarkerView.kt
+++ streetviewmarker/src/main/kotlin/com/github/alkurop/mylibrary/StreetMarkerView.kt
@@ -3,14 +3,71 @@ package com.github.alkurop.mylibrary
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.widget.FrameLayout
+import android.widget.LinearLayout
+import com.github.alkurop.mylibrary.components.IStreetOverlayView
+import com.github.alkurop.mylibrary.components.StreetOverlayView
+import com.github.alkurop.mylibrary.components.TouchOverlayView
 import com.google.android.gms.maps.StreetViewPanoramaView
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera
+import java.util.*
 
 /**
  * Created by alkurop on 2/3/17.
  */
-class StreetMarkerView : View {
-    lateinit var streetView: StreetViewPanoramaView
+class StreetMarkerView : LinearLayout , IStreetOverlayView{
 
-    @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0)
-              : super(context, attrs, defStyleAttr, defStyleRes)
+
+  val overlay: StreetOverlayView
+  val streetView: StreetViewPanoramaView
+  val touchOverlay: TouchOverlayView
+  var onSteetLoadedSuccess : ((Boolean) -> Unit)? = null
+
+  @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0, defStyleRes: Int = 0)
+      : super(context, attrs, defStyleAttr, defStyleRes) {
+    inflate(context, R.layout.view_street_marker, this)
+    overlay = findViewById(R.id.panorama) as StreetOverlayView
+    streetView = findViewById(R.id.panorama) as StreetViewPanoramaView
+    touchOverlay = findViewById(R.id.touch) as TouchOverlayView
+    touchOverlay.onTouchListener = {
+      overlay.onTouchEvent(it)
+    }
+    streetView.getStreetViewPanoramaAsync { panorama ->
+      panorama.setOnStreetViewPanoramaCameraChangeListener { cameraPosition ->
+        overlay.onCameraUpdate(cameraPosition)
+      }
+      panorama.setOnStreetViewPanoramaChangeListener { cameraPosition ->
+        if (cameraPosition != null && cameraPosition.position != null) {
+          overlay.onLocationUpdate(cameraPosition.position)
+          sendCameraPosition(cameraPosition.position)
+        }
+        onSteetLoadedSuccess?.invoke(cameraPosition != null && cameraPosition.links != null)
+      }
+    }
+  }
+
+  override fun onLocationUpdate(location: LatLng) {
+    overlay.onLocationUpdate(location)
+  }
+
+  override fun onCameraUpdate(cameraPosition: StreetViewPanoramaCamera) {
+    overlay.onCameraUpdate(cameraPosition)
+  }
+
+  override fun addMarkers(markers: HashSet<Place>) {
+    overlay.addMarkers(markers)
+  }
+
+  override fun onClick() {
+    overlay.onClick()
+  }
+
+  override fun setOnMarkerClickListener(onClickListener: ((data: MarkerDrawData) -> Unit)?) {
+    overlay.setOnMarkerClickListener (onClickListener)
+  }
+
+  private fun sendCameraPosition(position: LatLng?) {
+
+  }
 }
